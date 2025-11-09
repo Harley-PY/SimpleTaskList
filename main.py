4f808819a48aaa99df36581a744473ac448be155
Harley-PY <242923326+Harley-PY@users.noreply.github.com>
2025-11-09 11:40:15 +0000

    Refactor

--- main.py
+++ main.py
@@ -7,19 +7,37 @@ if not os.path.isfile("tasks.json"):
     with open("tasks.json", "w") as tasks:
         tasks.write("{\"tasks\": []}") # create blank json file
 
-def write_json(new_data, filename='tasks.json'):
-    with open(filename, 'r+') as file:
-        # Load existing data into a dictionary
-        file_data = json.load(file)
+class task_manager:
+    def read(isPretty=False):
+        with open("tasks.json", "r") as file:
+            js = json.loads(file.read())
+            tasks = js["tasks"]
 
-        # Append new data to the 'emp_details' list
-        file_data["tasks"].append(new_data)
+            if isPretty:
+                final_string = ""
 
-        # Move the cursor to the beginning of the file
-        file.seek(0)
+                i = 1
+                for task in tasks:
+                    final_string = f"{final_string}{str(i)}. {task["task_name"]}\n"
+                    i += 1
 
-        # Write the updated data back to the file
-        json.dump(file_data, file, indent=4)
+                return final_string
+
+            return tasks
+
+    def new(new_data):
+        with open("tasks.json", 'r+') as file:
+            # Load existing data into a dictionary
+            file_data = json.load(file)
+
+            # Append new data to the 'emp_details' list
+            file_data["tasks"].append(new_data)
+
+            # Move the cursor to the beginning of the file
+            file.seek(0)
+
+            # Write the updated data back to the file
+            json.dump(file_data, file, indent=4)
 
 while True:
     print("What would you like to do?")
@@ -30,25 +48,17 @@ while True:
     selection = input("Enter selection: ").lower()
 
     match selection:
-        case "r":
-            with open("tasks.json", "r") as file:
-                js = json.loads(file.read())
-                tasks = js["tasks"]
-
-                i = 1
-                for task in tasks:
-                    print(str(i) + ".", task["task_name"])
-                    i += 1
-        case "m":
-            with open("tasks.json", "r") as tasks:
-                js = json.loads(tasks.read())
-                print(js)
-        case "n":
+        case "r": ## read tasks
+            output = task_manager.read(isPretty=True)
+            print(output)
+        case "m": ## modify task
+            print("Not implemented")
+        case "n": ## new task
             while True:
                 task_name = input("What is the name of the task: ")
 
                 if len(task_name) != 0:
-                        write_json({"task_name": task_name})
+                        task_manager.new({"task_name": task_name})
                         print("Success!")
                         break
                 else:
